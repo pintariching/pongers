b7e10bcb8bc2e70d29d79b56f29503c408d6579c
pintariching <tilen.pintaric99@gmail.com>
2023-10-16 22:37:25 +0200

    add paddle logic

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,46 @@
-use bevy::prelude::{App, Camera2dBundle, Commands, FixedTime, Startup};
+use bevy::prelude::*;
 use bevy::DefaultPlugins;
 use bevy_xpbd_2d::prelude::PhysicsPlugins;
+
+mod paddle;
+
+use paddle::Paddle;
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
         .insert_resource(FixedTime::new_from_secs(1. / 60.))
         .add_systems(Startup, setup)
+        .add_systems(Update, update)
         .run();
 }
 
 fn setup(mut commands: Commands) {
+    commands.spawn_batch(vec![
+        Paddle {
+            width: 50,
+            height: 200,
+            position: Vec2::new(50., 50.),
+            rotation: 0.,
+            up: KeyCode::W,
+            down: KeyCode::S,
+        },
+        Paddle {
+            width: 50,
+            height: 200,
+            position: Vec2::new(-50., -50.),
+            rotation: 0.,
+            up: KeyCode::Up,
+            down: KeyCode::Down,
+        },
+    ]);
+
     commands.spawn(Camera2dBundle::default());
+}
+
+fn update(input: Res<Input<KeyCode>>, time: Res<Time>, mut query: Query<&mut Paddle>) {
+    for mut paddle in query.iter_mut() {
+        paddle.handle_input(&input, time.delta_seconds());
+        println!("{:#?}", paddle);
+    }
 }
--- src/paddle.rs
+++ src/paddle.rs
@@ -1,9 +1,23 @@
-use bevy::prelude::{App, Plugin};
+use bevy::prelude::*;
 
-pub struct Paddle {}
+#[derive(Debug, Component)]
+pub struct Paddle {
+    pub width: u32,
+    pub height: u32,
+    pub position: Vec2,
+    pub rotation: f32,
+    pub up: KeyCode,
+    pub down: KeyCode,
+}
 
-impl Plugin for Paddle {
-    fn build(&self, app: &mut App) {
-        todo!()
+impl Paddle {
+    pub fn handle_input(&mut self, input: &Res<Input<KeyCode>>, delta_time: f32) {
+        if input.pressed(self.up) {
+            self.position.y += 100. * delta_time;
+        }
+
+        if input.pressed(self.down) {
+            self.position.y += -100. * delta_time;
+        }
     }
 }
