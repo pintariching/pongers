8c5afe5e234a99cca8ca8a57648d8e49d0c79b81
pintariching <tilen.pintaric99@gmail.com>
2023-10-17 18:34:20 +0200

    add paddle functionality

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,27 @@
 use bevy::prelude::*;
 use bevy::DefaultPlugins;
 use bevy_xpbd_2d::prelude::PhysicsPlugins;
+use paddle::setup_paddles;
+use paddle::update_paddle;
+use paddle::Paddle;
 
 mod paddle;
-
-use paddle::Paddle;
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
         .insert_resource(FixedTime::new_from_secs(1. / 60.))
-        .add_systems(Startup, setup)
-        .add_systems(Update, update)
+        .add_systems(Startup, (setup, setup_paddles))
+        .add_systems(Update, (update_paddle, update))
         .run();
 }
 
 fn setup(mut commands: Commands) {
-    commands.spawn_batch(vec![
-        Paddle {
-            width: 50,
-            height: 200,
-            position: Vec2::new(50., 50.),
-            rotation: 0.,
-            up: KeyCode::W,
-            down: KeyCode::S,
-        },
-        Paddle {
-            width: 50,
-            height: 200,
-            position: Vec2::new(-50., -50.),
-            rotation: 0.,
-            up: KeyCode::Up,
-            down: KeyCode::Down,
-        },
-    ]);
-
     commands.spawn(Camera2dBundle::default());
 }
 
-fn update(input: Res<Input<KeyCode>>, time: Res<Time>, mut query: Query<&mut Paddle>) {
-    for mut paddle in query.iter_mut() {
-        paddle.handle_input(&input, time.delta_seconds());
+fn update(query: Query<&Paddle>) {
+    for paddle in &query {
         println!("{:#?}", paddle);
     }
 }
--- src/paddle.rs
+++ src/paddle.rs
@@ -2,22 +2,48 @@ use bevy::prelude::*;
 
 #[derive(Debug, Component)]
 pub struct Paddle {
-    pub width: u32,
-    pub height: u32,
+    pub width: f32,
+    pub height: f32,
     pub position: Vec2,
     pub rotation: f32,
     pub up: KeyCode,
     pub down: KeyCode,
 }
 
-impl Paddle {
-    pub fn handle_input(&mut self, input: &Res<Input<KeyCode>>, delta_time: f32) {
-        if input.pressed(self.up) {
-            self.position.y += 100. * delta_time;
-        }
+pub fn setup_paddles(mut commands: Commands) {
+    commands.spawn((
+        Paddle {
+            width: 50.,
+            height: 200.,
+            position: Vec2::new(50., 50.),
+            rotation: 0.,
+            up: KeyCode::W,
+            down: KeyCode::S,
+        },
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.25, 0.25, 0.75),
+                custom_size: Some(Vec2::new(50., 200.)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(50., 50., 0.)),
+            ..default()
+        },
+    ));
+}
 
-        if input.pressed(self.down) {
-            self.position.y += -100. * delta_time;
-        }
+pub fn update_paddle(input: Res<Input<KeyCode>>, time: Res<Time>, mut query: Query<&mut Paddle>) {
+    for mut paddle in query.iter_mut() {
+        handle_paddle_input(&mut paddle, &input, time.delta_seconds());
+    }
+}
+
+fn handle_paddle_input(paddle: &mut Paddle, input: &Res<Input<KeyCode>>, delta_time: f32) {
+    if input.pressed(paddle.up) {
+        paddle.position.y += 100. * delta_time;
+    }
+
+    if input.pressed(paddle.down) {
+        paddle.position.y += -100. * delta_time;
     }
 }
